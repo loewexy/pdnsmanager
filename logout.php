7e673d45e041700d36555698d82ef2588ae1e3dd
Lukas Metzger <developer@lukas-metzger.com>
2016-01-23 11:36:27 +0100

    Fixed missing logout

--- logout.php
+++ logout.php
@@ -15,8 +15,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <?php
-    //require_once 'lib/session.php';
-    //session_destroy();
+    require_once 'lib/session.php';
+    session_destroy();
 ?>
 <html>
     <head>
