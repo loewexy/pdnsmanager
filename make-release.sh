0877c655cf972582524bcf6ad8f693474330508c
Lukas Metzger <developer@lukas-metzger.com>
2016-02-02 17:53:16 +0100

    Changed release script to use git tags

--- make-release.sh
+++ make-release.sh
@@ -2,4 +2,4 @@
 
 mkdir -p releases/
 
-tar -czf releases/dpns-manager-$( git rev-parse HEAD | cut -c 1-10 ).tar.gz *.php LICENSE lib/ js/ include/ api/ config/config-default.php
+tar -czf releases/pdns-manager-$( git describe | cut -c 2- ).tar.gz *.php LICENSE lib/ js/ include/ api/ config/config-default.php
