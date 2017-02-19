5c9f0da3aa5230df76c7da51964aa479b4b983f4
Lukas Metzger <developer@lukas-metzger.com>
2017-02-19 12:52:40 +0100

    Adding unique constraint on update

--- api/upgrade.php
+++ api/upgrade.php
@@ -134,6 +134,8 @@ if(isset($input->action) && $input->action == "requestUpgrade") {
             RENAME TABLE user TO users;
             ALTER TABLE permissions CHANGE user userid INT(11);
             
+            ALTER TABLE users ADD CONSTRAINT UNIQUE KEY user_name_index (name);
+
             UPDATE options SET value=4 WHERE name='schema_version';
         ";
         $sql["pgsql"] = "UPDATE options SET value=4 WHERE name='schema_version';";
