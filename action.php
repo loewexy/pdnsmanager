edf80e353bf4263a1ee4703f700429254ce2bf8f
root <root@dns-pdns>
2018-02-13 10:11:37 +0100

    add log of failed logins in apache error_log

---
+++ action.php
@@ -0,0 +1,11 @@
+<?php
+
+if(isset($_POST['action']) && !empty($_POST['action'])) {
+    $action = $_POST['action'];
+    switch($action) {
+        case 'failedlogin' :
+	    #error_log("login failed for user ".$_POST['user']." (".$_SERVER['REMOTE_ADDR']), 3, "/tmp/mes-erreurs.log");
+	    error_log("PDNS-MANAGER : login failed for user ".$_POST['user']." (".$_SERVER['REMOTE_ADDR'].")\n", 3, "/var/log/apache2/error.log");
+	break;
+    }
+}
