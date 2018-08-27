df99f5e23716c354c7db1a1ec59c50c96598d0cd
Lukas Metzger <developer@lukas-metzger.com>
2018-08-27 14:12:14 +0200

    Fixed travis config

--- .travis/build-package.sh
+++ .travis/build-package.sh
@@ -3,7 +3,7 @@
 
 if test $TRAVIS_TAG
 then
-    utils/make-package.sh pdnsmanager-$TRAVIS_TAG
+    utils/make-package.sh pdnsmanager-${TRAVIS_TAG:1}
     utils/make-package.sh pdnsmanager-$TRAVIS_COMMIT
 else
     utils/make-package.sh pdnsmanager-$TRAVIS_COMMIT
--- .travis/deploy-release.sh
+++ .travis/deploy-release.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -F "file=@pdnsmanager-$TRAVIS_TAG.tar.gz" -u "travis:$UPLOAD_PASS" 'https://upload.pdnsmanager.org/?action=release'
+curl -F "file=@pdnsmanager-${TRAVIS_TAG:1}.tar.gz" -u "travis:$UPLOAD_PASS" 'https://upload.pdnsmanager.org/?action=release'
